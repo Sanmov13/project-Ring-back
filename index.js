9f9fea801cb16c17a0a754e134857ae57c94c49a
Saluev <ibragim.saluev@mail.ru>
2022-09-20 10:53:50 +0300

    Бэк

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Подключились к базе");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.listen(process.env.PORT, () => {
+  console.log("Подключились к серверу");
+});
+
+start();
