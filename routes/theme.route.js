fe3df3acc1ebfd0da292f371a3f47df7ea832af3
Kant <kant@mail.ru>
2022-09-23 14:52:23 +0300

    Сделал удаление комментария

--- routes/theme.route.js
+++ routes/theme.route.js
@@ -2,11 +2,11 @@ const { themeController } = require('../controllers/theme.controller');
 const Router = require('express');
 const router = Router();
 const fileMiddleware = require('../middleware/file')
+const authMiddleware = require('../middleware/auth.middleware')
 
 router.get('/themes', themeController.getTheme);
-router.post('/theme', themeController.addTheme);
+router.post('/theme', authMiddleware, themeController.addTheme);
 router.patch('/theme/:id', themeController.addLike)
-
 router.post('/upload', fileMiddleware.single('avatar'), (req, res) => {
     try {
         if (req.file) {
